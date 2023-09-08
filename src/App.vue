cf1b61766f67f6c16f95554b2b18264ee8bc7b21
smilax630 <smilax630@gmail.com>
2023-09-08 23:24:05 +0900

    feat: cmd+enterで文字を追加できるように修正

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,117 @@
 <template>
   <v-grid 
+    ref="grid"
     theme="material"
     @click="handleClick($event)" 
     @keydown="handlePress($event)"
+    @beforecellfocus="focus($event)"
     :source="rows" 
     :columns="columns"
     :resize="true"
   />
 </template>
 
-<script>
+<script setup>
 import VGrid from "@revolist/vue3-datagrid";
 import { ref } from 'vue';
+const grid = ref()
 
-export default {
-  components: {
-    VGrid,
-  },
-  name: "App",
-  setup() {
-    const columns = [
-      {
-        name: 'Id',
-        prop: 'id',
-        cellTemplate: (createElement, props) => {
-          return createElement('span', {
-            style: {
-              color: 'black'
-            },
-          }, props.model[props.prop]);
+const columns = [
+  {
+    name: 'Id',
+    prop: 'id',
+    cellTemplate: (createElement, props) => {
+      console.log("apppppppppppp")
+      return createElement('div', {
+        style: {
+          color: 'black'
         },
-      },
-      {
-        name: 'badge',
-        prop: 'badge',
-        cellTemplate: (createElement, props) => {
-          return createElement('span', {
-            style: {
-              color: 'white',
-              backgroundColor: props.model[props.prop],
-              borderRadius: '5px',
-              padding: '5px',
-            },
-          }, props.model[props.prop]);
-        }
-      },
-      {
-        name: 'contents',
-        prop: 'contents',
-        cellTemplate: (createElement, props) => {
-
-          const str = props.model[props.prop];
-          const regex = /(https?:\/\/[^\s()（）]+)/g;
-          const parts = str.split(regex);
-
-          return createElement('dev', {}, parts.map(part => {
-              if (regex.test(part)) {
-                  return createElement('span', {
-                      style: {
-                          color: 'blue',
-                          textDecoration: 'underline'
-                      }
-                  }, part);
-              } else {
-                  return createElement('span', {}, part);
-              }
-          }));
+        repeat: true,
+        onkeydown: (event) => {
+          console.log("awefa")
+          console.log(event)
+          if (event.metaKey === true && event.code === 'Enter') {
+            event.preventDefault();
+          }
         },
-      },
-    ];
-
-
-    const rows = ref([
-      {
-        id: "1",
-        badge: "blue",
-        contents: "http://www.google.com",
-      },
-      {
-        id: "2",
-        badge: "red",
-        contents: "テストデータ",
-      },
-    ]);
-
-    function handleClick(event) {
-      const str = event.target.innerText;
-      if (event.metaKey && (str.includes('https://') || str.includes('http://'))) {
-        window.open(str)
-      }
-    }
-
-    function handlePress(event) {
-      console.log(event)
-    }
-
-    
-
-    return {
-      columns,
-      rows,
-      handlePress,
-      handleClick,
-    };
+        contenteditable: true,
+      }, props.model[props.prop]);
+    },
   },
-};
+  {
+    name: 'badge',
+    prop: 'badge',
+    cellTemplate: (createElement, props) => {
+      return createElement('span', {
+        style: {
+          color: 'white',
+          backgroundColor: props.model[props.prop],
+          borderRadius: '5px',
+          padding: '5px',
+        },
+      }, props.model[props.prop]);
+    }
+  },
+  {
+    name: 'contents',
+    prop: 'contents',
+    cellTemplate: (createElement, props) => {
+
+      const str = props.model[props.prop];
+      const regex = /(https?:\/\/[^\s()（）]+)/g;
+      const parts = str.split(regex);
+
+      return createElement('dev', {}, parts.map(part => {
+          if (regex.test(part)) {
+              return createElement('span', {
+                  style: {
+                      color: 'blue',
+                      textDecoration: 'underline'
+                  }
+              }, part);
+          } else {
+              return createElement('span', {}, part);
+          }
+      }));
+    },
+  },
+];
+
+const rows = ref([
+  {
+    id: "1",
+    badge: "blue",
+    contents: "http://www.google.com",
+  },
+  {
+    id: "2",
+    badge: "red",
+    contents: "テストデータ",
+  },
+]);
+
+function handleClick(event) {
+  const str = event.target.innerText;
+  if (event.metaKey && (str.includes('https://') || str.includes('http://'))) {
+    window.open(str)
+  }
+}
+
+function handlePress(event) {
+  if (event.metaKey === true && event.code === 'Enter') {
+    event.preventDefault();
+    const value = rows.value[focusedCell.value.rowIndex][focusedCell.value.prop];
+    rows.value[focusedCell.value.rowIndex][focusedCell.value.prop] = value + "追加された";
+    grid.value.$el.refresh('all')
+  }
+}
+
+// フォーカスがあったているセルの情報を保持する
+const focusedCell = ref();
+
+function focus(event) {
+  focusedCell.value = event.detail
+}
 </script>
 
 
@@ -116,5 +123,9 @@ export default {
 revo-grid {
   height: 100%;
   width: 100vw;
+}
+
+input {
+  white-space: pre-wrap;
 }
 </style>
